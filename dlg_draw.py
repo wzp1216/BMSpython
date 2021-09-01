6ea3b0b950085a8d4d18fc6058b3f9ea4b055d55
wzp1216@163.com <zlh720328>
2021-09-01 17:07:09 +0800

    add matplog_draw_test file

--- dlg_draw.py
+++ dlg_draw.py
@@ -1,49 +1,115 @@
 # -*- coding: utf-8 -*-
 # BMS  dlg_draw  show the voltage of battery;
-from PyQt5 import QtCore,QtGui,QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import *
+import sys
 import numpy as np
 import matplotlib
+
 matplotlib.use("Qt5Agg")
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 
+b1 = 1
+b2 = 1
+b3 = 1
+b4 = 1
+b5 = 1
+b6 = 1
+b7 = 1
+b8 = 1
+
+
 class myFigure(FigureCanvas):
-    def __init__(self,width=5,height=4,dpi=100):
-        self.fig=Figure(figsize=(width,height),dpi=dpi)
-        super(myFigure,self).__init__(self.fig)
-        self.axes=self.fig.add_subplot()
-    def plotsin(self):
-        t=np.arange(0,5,0.01)
-        s=2+np.sin(2*t+5)
-        self.axes.plot(t,s)
+    def __init__(self, width=5, height=4, dpi=100):
+        self.fig = Figure(figsize=(width, height), dpi=dpi)
+        super(myFigure, self).__init__(self.fig)
+        self.axes = self.fig.add_subplot()
+
+    def plot_vol(self):
+        global b1, b2
+        x = np.arange(0, 10, 0.01)
+        self.y1 = x
+        self.y2 = 3.7 + 0.01 * np.cos(2 * x)
+        self.axes.set_xlim([0, 10])
+        self.axes.set_ylim([0, 5])
+
+    def update_plot(self):
+        self.axes.plot(self.x, self.y1)
+        self.canvas.draw()
+
 
 class dlg_draw(QWidget):
-    def __init__(self,parent=None):
-        super(dlg_draw,self).__init__(parent)
+    def __init__(self, parent=None):
+        super(dlg_draw, self).__init__(parent)
         self.draw()
 
     def draw(self):
-        self.f=myFigure(width=3,height=2,dpi=100)
-        self.f.plotsin()
-        self.gridlayout=QGridLayout(self)
-        self.gridlayout.addWidget(self.f,0,0,9,8)
-        self.check_b1=QCheckBox("通道1")
-        self.check_b2=QCheckBox("通道2")
-        self.check_b3=QCheckBox("通道3")
-        self.check_b4=QCheckBox("通道4")
-        self.check_b5=QCheckBox("通道5")
-        self.check_b6=QCheckBox("通道6")
-        self.check_b7=QCheckBox("通道7")
-        self.check_b8=QCheckBox("通道8")
-        self.gridlayout.addWidget(self.check_b1,0,10)
-        self.gridlayout.addWidget(self.check_b2,1,10)
-        self.gridlayout.addWidget(self.check_b3,2,10)
-        self.gridlayout.addWidget(self.check_b4,3,10)
-        self.gridlayout.addWidget(self.check_b5,4,10)
-        self.gridlayout.addWidget(self.check_b6,5,10)
-        self.gridlayout.addWidget(self.check_b7,6,10)
-        self.gridlayout.addWidget(self.check_b8,7,10)
+        self.f = myFigure(width=3, height=2, dpi=100)
+        self.f.plot_vol()
+        self.f.update_plot()
+        self.gridlayout = QGridLayout(self)
+        self.gridlayout.addWidget(self.f, 0, 0, 9, 8)
+        self.check_b1 = QCheckBox("通道1")
+        self.check_b2 = QCheckBox("通道2")
+        self.check_b3 = QCheckBox("通道3")
+        self.check_b4 = QCheckBox("通道4")
+        self.check_b5 = QCheckBox("通道5")
+        self.check_b6 = QCheckBox("通道6")
+        self.check_b7 = QCheckBox("通道7")
+        self.check_b8 = QCheckBox("通道8")
+        self.gridlayout.addWidget(self.check_b1, 0, 10)
+        self.gridlayout.addWidget(self.check_b2, 1, 10)
+        self.gridlayout.addWidget(self.check_b3, 2, 10)
+        self.gridlayout.addWidget(self.check_b4, 3, 10)
+        self.gridlayout.addWidget(self.check_b5, 4, 10)
+        self.gridlayout.addWidget(self.check_b6, 5, 10)
+        self.gridlayout.addWidget(self.check_b7, 6, 10)
+        self.gridlayout.addWidget(self.check_b8, 7, 10)
 
-    
+        self.check_b1.stateChanged.connect(self.check_changed)
+        self.check_b2.stateChanged.connect(self.check_changed)
+        self.check_b3.stateChanged.connect(self.check_changed)
+        self.check_b4.stateChanged.connect(self.check_changed)
+        self.check_b5.stateChanged.connect(self.check_changed)
+        self.check_b6.stateChanged.connect(self.check_changed)
+        self.check_b7.stateChanged.connect(self.check_changed)
+        self.check_b8.stateChanged.connect(self.check_changed)
+
+    def check_changed(self):
+        global b1, b2, b3, b4, b5, b6, b7, b8
+        if True == self.check_b1.isChecked():
+            b1 = 1
+        else:
+            b1 = 0
+        if True == self.check_b2.isChecked():
+            b2 = 1
+        else:
+            b2 = 0
+        if True == self.check_b3.isChecked():
+            b3 = 3
+        else:
+            b3 = 0
+        if True == self.check_b4.isChecked():
+            b4 = 4
+        else:
+            b4 = 0
+        if True == self.check_b5.isChecked():
+            b5 = 5
+        else:
+            b5 = 0
+        if True == self.check_b6.isChecked():
+            b6 = 6
+        else:
+            b6 = 0
+        if True == self.check_b7.isChecked():
+            b7 = 7
+        else:
+            b7 = 0
+        if True == self.check_b8.isChecked():
+            b8 = 8
+        else:
+            b8 = 0
+        self.f.plot_vol()
+        self.f.update_plot()
--- matplot_test.py
+++ matplot_test.py
@@ -1,13 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-np.random.seed(19680801)
-data = np.random.randn(2, 100)
 
-fig, axs = plt.subplots(2, 2, figsize=(5, 5))
-axs[0, 0].hist(data[0])
-axs[1, 0].scatter(data[0], data[1])
-axs[0, 1].plot(data[0], data[1])
-axs[1, 1].hist2d(data[0], data[1])
+x = np.arange(0, 10, 0.01)
+y = x
+plt.plot(x,y)
 
 plt.show()
