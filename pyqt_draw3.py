3f689b440e1595aae4d51121436164e7c5e51469
wzp1216 <wzp1216@163.com>
2021-09-02 22:10:00 +0800

    add draw3 and ok

---
+++ pyqt_draw3.py
@@ -0,0 +1,69 @@
+import sys
+import random
+import numpy as np
+import array
+import random
+import pyqtgraph as pg
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QPushButton, QVBoxLayout
+from PyQt5.QtCore import QTimer
+
+idx=0
+rand=random.randint(50,300)
+class main(QWidget):
+    def __init__(self):
+        super(main, self).__init__()
+        self.draw1()
+        self.draw2()
+        self.initui()
+    def draw1(self):
+        self.plt1=pg.PlotWidget()
+        x=np.random.normal(size=1000)
+        y=np.random.normal(size=1000)
+        self.plt1.plot(x,y,pen=None,symbol='o')
+
+    def draw2(self):
+        self.plt2=pg.PlotWidget()
+        self.tim1=QTimer()
+        self.tim1.timeout.connect(self.update)
+        self.tim1.start(100)
+
+        self.x=500
+        self.plt2.setRange(xRange=[0,self.x],yRange=[0,5],padding=0)
+        self.dat=array.array('d')
+        self.curve=self.plt2.plot(pen='y')
+
+
+    def update(self):
+        global idx
+        global rand
+        if idx<rand:
+            tmp1=3.8
+        else:
+            idx=0
+            rand=random.randint(50,300)
+            tmp1=3.9
+        if len(self.dat)<self.x:
+            self.dat.append(tmp1)
+        else:
+            self.dat[:-1]=self.dat[1:]
+            self.dat[-1]=tmp1
+        self.curve.setData(self.dat)
+        idx+=1;
+
+
+    def initui(self):
+        self.resize(1200, 1000)
+        label1=QLabel("test the pyqtgraph")
+        layout=QVBoxLayout()
+        layout.addWidget(label1)
+        layout.addWidget(self.plt1)
+        layout.addWidget(self.plt2)
+        self.setLayout(layout)
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win= main()
+    win.show()
+    sys.exit(app.exec_())
